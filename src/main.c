a757c2a5ffd6a7b90ba60314350b306ebeb5e797
indigoparadox <indigo.repo@interfinitydynamics.info>
2024-08-10 22:47:35 -0400

    Added stepping.

--- src/main.c
+++ src/main.c
@@ -14,8 +14,11 @@
 #include <mlispp.h>
 #include <mlispe.h>
 
+#define MLISP_FLAG_DO_STEP 0x02
+
 struct MLISP_DATA {
    int init;
+   uint8_t flags;
    struct MLISP_PARSER parser;
    struct MLISP_EXEC_STATE exec;
    MAUG_MHANDLE font_h;
@@ -24,7 +27,7 @@ struct MLISP_DATA {
 
 MERROR_RETVAL mlisp_cb_write(
    struct MLISP_PARSER* parser, struct MLISP_EXEC_STATE* exec, size_t n_idx,
-   void* cb_data, uint8_t flags
+   size_t args_c, void* cb_data, uint8_t flags
 ) {
    MERROR_RETVAL retval = MERROR_OK;
    struct MLISP_STACK_NODE val;
@@ -57,12 +60,18 @@ void mlisp_loop( struct MLISP_DATA* data ) {
    RETROFLAT_IN_KEY input = 0;
    struct RETROFLAT_INPUT input_evt;
    MERROR_RETVAL retval = MERROR_EXEC;
+   uint8_t do_next =
+      (MLISP_FLAG_DO_STEP == (MLISP_FLAG_DO_STEP & data->flags) ? 0 : 1 );
 
    /* Input */
 
    input = retroflat_poll_input( &input_evt );
 
    switch( input ) {
+   case RETROFLAT_KEY_SPACE:
+      do_next = 1;
+      break;
+
    case RETROFLAT_KEY_ESC:
       retroflat_quit( 0 );
       break;
@@ -72,17 +81,20 @@ void mlisp_loop( struct MLISP_DATA* data ) {
 
    retroflat_draw_lock( NULL );
 
-   if(
-      data->do_exec &&
-      (retval = mlisp_step( &(data->parser), &(data->exec) ))
-   ) {
-      mlisp_stack_dump( &(data->parser), &(data->exec) );
-      mlisp_env_dump( &(data->parser), &(data->exec) );
-      data->do_exec = 0;
-   } else if( MERROR_OK == retval ) {
-      mlisp_ast_dump( &(data->parser), 0, 0, 0 );
-      mlisp_stack_dump( &(data->parser), &(data->exec) );
-      mlisp_env_dump( &(data->parser), &(data->exec) );
+   if( do_next ) {
+      if(
+         data->do_exec &&
+         (retval = mlisp_step( &(data->parser), &(data->exec) ))
+      ) {
+         mlisp_stack_dump( &(data->parser), &(data->exec) );
+         mlisp_env_dump( &(data->parser), &(data->exec) );
+         data->do_exec = 0;
+
+      } else if( MERROR_OK == retval ) {
+         mlisp_ast_dump( &(data->parser), 0, 0, 0 );
+         mlisp_stack_dump( &(data->parser), &(data->exec) );
+         mlisp_env_dump( &(data->parser), &(data->exec) );
+      }
    }
 
    /*
@@ -93,6 +105,11 @@ void mlisp_loop( struct MLISP_DATA* data ) {
    */
 
    retroflat_draw_release( NULL );
+}
+
+static int mlisp_cli_step( const char* arg, struct MLISP_DATA* data ) {
+   data->flags |= MLISP_FLAG_DO_STEP;
+   return RETROFLAT_OK;
 }
 
 int main( int argc, char** argv ) {
@@ -110,21 +127,26 @@ int main( int argc, char** argv ) {
 
    logging_init();
 
-   memset( &args, '\0', sizeof( struct RETROFLAT_ARGS ) );
-
-   args.title = "mlisp";
-   args.assets_path = "assets";
-   
-   retval = retroflat_init( argc, argv, &args );
-   if( RETROFLAT_OK != retval ) {
-      goto cleanup;
-   }
-
    data_h = maug_malloc( 1, sizeof( struct MLISP_DATA ) );
    maug_cleanup_if_null_alloc( MAUG_MHANDLE, data_h );
    maug_mlock( data_h, data );
    maug_cleanup_if_null_alloc( struct MLISP_DATA*, data );
    maug_mzero( data, sizeof( struct MLISP_DATA ) );
+
+   memset( &args, '\0', sizeof( struct RETROFLAT_ARGS ) );
+
+   args.title = "mlisp";
+   args.assets_path = "assets";
+
+	retval = maug_add_arg( MAUG_CLI_SIGIL "s", MAUG_CLI_SIGIL_SZ + 1,
+      "Step through script with SPACE.", 0,
+      (maug_cli_cb)mlisp_cli_step, NULL, data );
+   maug_cleanup_if_not_ok();
+   
+   retval = retroflat_init( argc, argv, &args );
+   if( RETROFLAT_OK != retval ) {
+      goto cleanup;
+   }
 
    retval = retrofont_load(
       "unscii-8.hex", &(data->font_h), 0, 33, 93 );
@@ -157,14 +179,16 @@ int main( int argc, char** argv ) {
       data, MLISP_ENV_FLAG_BUILTIN );
    maug_cleanup_if_not_ok();
 
-   debug_printf( 1, "mlisp uses: " SIZE_T_FMT " bytes RAM",
+   debug_printf( 1,
+      "mlisp parser uses: " SIZE_T_FMT ", exec uses: " SIZE_T_FMT " bytes RAM",
       sizeof( struct MLISP_PARSER ) +
-      mdata_vector_sz( &(data->parser.ast) ) +
-      mdata_strpool_sz( &(data->parser.strpool) ) +
+         mdata_vector_sz( &(data->parser.ast) ) +
+         mdata_strpool_sz( &(data->parser.strpool) ),
       sizeof( struct MLISP_EXEC_STATE ) +
-      mdata_vector_sz( &(data->exec.per_node_child_idx) ) +
-      mdata_vector_sz( &(data->exec.stack) ) +
-      mdata_vector_sz( &(data->exec.env) ) );
+         mdata_vector_sz( &(data->exec.per_node_child_idx) ) +
+         mdata_vector_sz( &(data->exec.stack) ) +
+         mdata_vector_sz( &(data->exec.lambda_trace) ) +
+         mdata_vector_sz( &(data->exec.env) ) );
 
    data->do_exec = 1;
 
--- test.lisp
+++ test.lisp
@@ -1,15 +1,16 @@
 
 (begin
-   (define plusfour
-      (lambda (y z)
-         (if (< z 4) (plusthree (+ y 1) (+ z 1)) y)))
+   ; This is a comment.
    (define plusthree
       (lambda (y z)
          (if (< z 3) (plusthree (+ y 1) (+ z 1)) y)))
+   (define plusn
+      (lambda (y z q)
+         (plusthree y z)))
    (define pi 3.14)
    (define r 10)
-   (define c (* pi (* r r)))
-   (define d (plusthree 8 0))
+   (define c (+ -1 (* pi (* r r))))
+   (define d (plusn 8 0 3))
    (begin
       (write d)))
 
