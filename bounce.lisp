850e20a7971b82ba26641fc5add7fd5262579c55
indigoparadox <indigo.repo@interfinitydynamics.info>
2024-08-20 19:18:20 -0400

    Fixed bounce demo.

--- bounce.lisp
+++ bounce.lisp
@@ -5,9 +5,13 @@
          (rectf black 0 0 320 200)
          (rect blue x y 10 10)
          (define vx
-            (if (> (+ x vx) 310) -10 10))
+            (if 
+               (or (> (+ x vx) 310) (< (+ x vx) 0))
+               (* vx -1) vx))
          (define vy
-            (if (> (+ y vy) 190) -10 10))
+            (if
+               (or (> (+ y vy) 190) (< (+ y vy) 0))
+               (* vy -1) vy))
          (fly (+ x vx) (+ y vy) vx vy)))
    (fly 0 0 10 10))
 
